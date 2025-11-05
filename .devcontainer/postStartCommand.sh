d9a1e3b28202e1dbb90daee366fec85b49b0a424
takamt <tkn.moto@gmail.com>
2025-11-06 01:12:05 +0900

    chore: migrate to docker-compose and restructure project

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-# Wait for dotfiles installation to finish, then switch terminal to zsh
-bash -c \
-    'while [ ! -f ~/.dotfiles-installed ]; do \
-        echo \"Waiting for dotfiles installation...\"; \
-        sleep 1; \
-    done; \
-    echo \"Dotfiles installation completed!\"' \
-    && zsh
+# Wait for dotfiles installation to finish.
+if [ -d "$HOME/dotfiles" ]; then
+    bash -c \
+        'while [ ! -f ~/.dotfiles-installed ]; do \
+            echo \"Waiting for dotfiles installation...\"; \
+            sleep 1; \
+        done;'
+    echo "Dotfiles installation completed!"
+else
+    echo "No dotfiles directory found. You can use the 'dotfiles' feature."
+fi
+
+# Then switch terminal to $SHELL
+SHELL_PATH=$(command -v $SHELL)
+if [ -n "$SHELL_PATH" ]; then
+    echo "$SHELL is installed. Switching to $SHELL..."
+    sudo chsh -s "$SHELL_PATH" "$USERNAME"
+    exec "$SHELL"
+else
+    echo "$SHELL is not installed. Staying with the default shell."
+fi
