ec18faa9853a795602d5744052252620220ab026
takamt <tkn.moto@gmail.com>
2025-11-08 22:13:54 +0900

    chore(app/.devcontainer): postStartCommand.sh - No wait for dotfiles installation

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# Wait for dotfiles installation to finish.
-if [ -d "$HOME/dotfiles" ]; then
-    bash -c \
-        'while [ ! -f ~/.dotfiles-installed ]; do \
-            echo \"Waiting for dotfiles installation...\"; \
-            sleep 1; \
-        done;'
-    echo "Dotfiles installation completed!"
-else
-    echo "No dotfiles directory found. You can use the 'dotfiles' feature."
-fi
-
 # Then switch terminal to $SHELL
 SHELL_PATH=$(command -v $SHELL)
 if [ -n "$SHELL_PATH" ]; then
