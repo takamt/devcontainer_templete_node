09c5d3f2ea3e9ae62b225399f405d820266e4728
takamt <tkn.moto@gmail.com>
2025-11-09 14:19:31 +0900

    refacter(app/.devcontainer): postAttacheCommand.sh - update comments

--- .devcontainer/postAttachCommand.sh
+++ .devcontainer/postAttachCommand.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Then switch terminal to $SHELL
+
+# switch terminal to $SHELL
+echo "Checking if $SHELL is installed..."
 SHELL_PATH=$(command -v $SHELL)
 if [ -n "$SHELL_PATH" ]; then
     echo "$SHELL is installed. Switching to $SHELL..."
     exec "$SHELL"
+    # as switching shells, the rest of this script will not be executed
 else
     echo "$SHELL is not installed. Staying with the default shell."
 fi
